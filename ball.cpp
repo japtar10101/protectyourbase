43cd032edca73d4e0f308981373c685f4c74aadd
Japtar10101 <Japtar10101@c4e27832-a6e1-11dd-b655-b92e15b8dc27>
2008-11-05 22:21:55 +0000

    First time working on the actual project.

---
+++ ball.cpp
@@ -0,0 +1,30 @@
+#include "ball.h"
+
+Ball::Ball() {
+	x = 0;
+	y = 0;
+	x_move = 0.0000000001;
+	y_move = 0.0000000001;
+}
+
+Ball::Ball( int x1, int y1 ) {
+	x = x1;
+	y = y1;
+}
+
+void Ball::draw_ball() {
+	x += x_move;
+	y += y_move;
+	glColor3f( 1.0, 1.0, 1.0 );
+	glBegin(GL_POLYGON);
+	for( int index = 0; index < 360; ++index ) {
+		float degInRad = index * DEGR_TO_RADIANS;
+		float x_coord = cos( degInRad );
+		x_coord = x_coord * RADIUS + x;
+		float y_coord = sin( degInRad );
+		y_coord = y_coord * RADIUS + y;
+		glVertex2f( x_coord, y_coord );
+	}
+	glEnd();
+	glutPostRedisplay();
+}
