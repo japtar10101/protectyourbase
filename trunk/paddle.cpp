bc74897f140d4b994465ccbd6bb30be9c7944b53
Japtar10101 <Japtar10101@c4e27832-a6e1-11dd-b655-b92e15b8dc27>
2008-11-23 22:18:20 +0000

    Adding Paddle files.

---
+++ trunk/paddle.cpp
@@ -0,0 +1,153 @@
+#include "paddle.h"
+
+/**** LOTS of constructors ****/
+
+Paddle::Paddle() :
+Block(), color(), horizontal( true ) {
+	lower_limit = x;
+	upper_limit = x + default_range;
+}
+
+Paddle::Paddle( float range_of_movement, bool move_horizontal ) :
+Block(), color(), horizontal( move_horizontal ) {
+	if( horizontal ) {
+		lower_limit = x;
+		upper_limit = x + range_of_movement;
+	} else {
+		lower_limit = y;
+		upper_limit = y + range_of_movement;
+	}
+}
+
+Paddle::Paddle( float range_of_movement, bool move_horizontal,
+	float red, float green, float blue ) :
+Block(),
+color( red, green, blue ),
+horizontal( move_horizontal ) {
+	if( horizontal ) {
+		lower_limit = x;
+		upper_limit = x + range_of_movement;
+	} else {
+		lower_limit = y;
+		upper_limit = y + range_of_movement;
+	}
+}
+
+Paddle::Paddle( float x_coord, float y_coord,
+	float range_of_movement, bool move_horizontal ) :
+Block( x_coord, y_coord ),
+color(),
+horizontal( move_horizontal ) {
+	if( horizontal ) {
+		lower_limit = x_coord;
+		upper_limit = x_coord + range_of_movement;
+	} else {
+		lower_limit = y_coord;
+		upper_limit = y_coord + range_of_movement;
+	}
+}
+
+Paddle::Paddle( float x_coord, float y_coord,
+	float set_width, float set_height,
+	float range_of_movement, bool move_horizontal ) :
+Block( x_coord, y_coord, set_width, set_height ),
+color(),
+horizontal( move_horizontal ) {
+	if( horizontal ) {
+		lower_limit = x_coord;
+		upper_limit = x_coord + range_of_movement;
+	} else {
+		lower_limit = y_coord;
+		upper_limit = y_coord + range_of_movement;
+	}
+}
+
+Paddle::Paddle( float x_coord, float y_coord,
+	float set_width, float set_height,
+	float range_of_movement, bool move_horizontal,
+	float red, float green, float blue ) :
+Block( x_coord, y_coord, set_width, set_height ),
+color( red, green, blue ),
+horizontal( move_horizontal ) {
+	if( horizontal ) {
+		lower_limit = x_coord;
+		upper_limit = x_coord + range_of_movement;
+	} else {
+		lower_limit = y_coord;
+		upper_limit = y_coord + range_of_movement;
+	}
+}
+
+/**** Getter and Setter ****/
+
+Color &Paddle::get_color() {
+	return color;
+}
+
+void Paddle::set_upper_limit( float limit ) {
+	if( limit < 0.0 ) limit = 0.0;
+	if( limit < lower_limit ) upper_limit = lower_limit;
+	else upper_limit = limit;
+}
+
+void Paddle::set_lower_limit( float limit ) {
+	if( limit < 0.0 ) limit = 0.0;
+	if( limit > upper_limit ) lower_limit = upper_limit;
+	else lower_limit = limit;
+}
+
+void Paddle::set_limit( float lower, float upper ) {
+	if( lower < 0.0 ) lower = 0.0;
+	if( upper < 0.0 ) upper = 0.0;
+	if( lower > upper ) lower_limit = upper_limit = lower;
+	else {
+		lower_limit = lower;
+		upper_limit = upper;
+	}
+}
+
+/**** functions that moves the paddle ****/
+
+void Paddle::move_up() {
+	if( !horizontal ) {
+		y += velocity;
+		//make sure y is in range
+		if( y > upper_limit )
+			y = upper_limit;
+	}
+}
+
+void Paddle::move_down() {
+	if( !horizontal ) {
+		y -= velocity;
+		//make sure y is in range
+		if( y > lower_limit )
+			y = lower_limit;
+	}
+}
+
+void Paddle::move_right() {
+	if( horizontal ) {
+		x += velocity;
+		//make sure x is in range
+		if( x > upper_limit )
+			x = upper_limit;
+	}
+}
+
+void Paddle::move_left() {
+	if( horizontal ) {
+		x -= velocity;
+		//make sure x is in range
+		if( x > upper_limit )
+			x = upper_limit;
+	}
+}
+
+/**** Functions to override ****/
+
+void Paddle::force_draw() {
+	color.color();
+	Block::force_draw();
+}
+
---
+++ trunk/paddle.h
@@ -0,0 +1,58 @@
+#ifndef PADDLE_H
+#define PADDLE_H
+
+#include "block.h"
+#include "color.h"
+
+class Paddle : public Block {
+private:
+	static const float velocity = 0.1;
+	static const float default_range = 10.0;
+	Color color;
+	//limits: the range the paddle can move
+	float upper_limit, lower_limit;
+	//vertical: if true, moves horizontal; false, vertical
+	bool horizontal;
+	
+public:
+	//constructor
+	Paddle();
+	Paddle( float range_of_movement, bool move_horizontal );
+	Paddle( float range_of_movement, bool move_horizontal,
+		float red, float green, float blue );
+	Paddle( float x_coord, float y_coord,
+		float range_of_movement, bool move_horizontal );
+	Paddle( float x_coord, float y_coord,
+		float set_width, float set_height,
+		float range_of_movement, bool move_horizontal );
+	Paddle( float x_coord, float y_coord,
+		float set_width, float set_height,
+		float range_of_movement, bool move_horizontal,
+		float red, float green, float blue );
+	
+	//getter and setter
+	bool get_horizontal()	{ return horizontal; }
+	float get_upper_limit()		{ return upper_limit; }
+	float get_lower_limit()		{ return lower_limit; }
+	Color &get_color();
+	void set_horizontal( bool change ) { horizontal = change; }
+	void set_upper_limit( float limit );
+	void set_lower_limit( float limit );
+	void set_limit( float lower, float upper );
+	
+	//more intuitive getters
+	bool moves_horizontally()	{ return horizontal; }
+	bool moves_vertically()		{ return !horizontal; }
+	
+	//functions that moves the paddle
+	void move_up();
+	void move_down();
+	void move_right();
+	void move_left();
+	
+	//override function
+	void force_draw();
+};
+
+#endif
+
