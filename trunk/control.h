a86d64d4411900ba7a7f1741f59404cd52d39509
Japtar10101 <Japtar10101@c4e27832-a6e1-11dd-b655-b92e15b8dc27>
2008-11-25 05:28:19 +0000

    I forgot these files.

---
+++ trunk/control.h
@@ -0,0 +1,30 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+#include "global.h"
+
+class Control {
+private:
+	unsigned char controls[8];
+	bool conditions[4];
+	
+public:
+	//Enumerator for movement
+	enum Move { up = 0, down, right, left };
+	
+	//Constructor
+	Control( bool player_one );
+	
+	//functions for keyboard
+	bool push_key( unsigned char key );
+	bool raise_key( unsigned char key );
+	
+	//function that gets the key conditions
+	bool get_key_condition( Move control ) { return conditions[control]; }
+	
+	//function that says that at least one key is pressed
+	bool any_key_pressed();
+};
+
+#endif
+
